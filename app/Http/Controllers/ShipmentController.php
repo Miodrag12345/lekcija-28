0429c0fb1d07056bb1a1dab104e57d1d18c50595
Miodrag12345 <realsosijedad@gmail.com>
2026-05-17 20:26:21 +0200

    update 30 lekcija

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -25,51 +25,39 @@ class ShipmentController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         return view('shipments.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(NewShipmentRequest $request)
     {
         Shipments::create($request->validated());
         return redirect()->route('shipments.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Shipments $shipment)
     {
         return view('shipments.show', compact($shipment));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Shipments $shipment)
     {
         return view('shipments.edit' , compact('shipment'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(UpdateShipmentRequest $request, Shipments $shipment)
     {
        $shipment->update($request->validated());
        return redirect()->back();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Shipments $shipments)
     {
         //
---
+++ app/Policies/ShipmentPolicy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Shipment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ShipmentPolicy
+{
+
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+
+    public function view(User $user, Shipment $shipment): bool
+    {
+        return false;
+    }
+
+
+    public function create(User $user): bool
+    {
+
+           return $user->role === User::ROLE_ADMINISTRATOR;
+    }
+
+
+    public function update(User $user, Shipment $shipment): bool
+    {
+        return false;
+    }
+
+
+    public function delete(User $user, Shipment $shipment): bool
+    {
+        return false;
+    }
+
+
+    public function restore(User $user, Shipment $shipment): bool
+    {
+        return false;
+    }
+
+
+    public function forceDelete(User $user, Shipment $shipment): bool
+    {
+        return false;
+    }
+
+    public function canViewCreationPage(User $user):bool
+    {
+        return $user->role === User::ROLE_ADMINISTRATOR;
+    }
+}
